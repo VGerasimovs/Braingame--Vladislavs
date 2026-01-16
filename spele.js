b77d4faefd6a4fdd7b69ff67f51d596741d83ad7
VGerasimovs <vgerasimovs1@anip.lv>
2026-01-16 09:09:19 +0200

    532532

--- spele.js
+++ spele.js
@@ -1,3 +1,22 @@
 //no URL iegūst vārdu
 let adrese = window.location.hash.substring(1);
 adrese = decodeURI(adrese.split(',')[0])
+
+//mainīgie spēles darbībai
+let laiks = 0; //sekundes
+let klikski = 0;
+
+//taimera mainīgie (taimeris strādās ar pirmo klikšķi)
+let timerid = null;
+let timerstarted = false;
+
+
+
+
+
+const laukumiSaturs = ['😚','😥','🤫','😥','👺','😚','👽','😵','👽','🤫','😵','👺']
+let atvertiLaukumi = [];
+let pedejieDivi = []; 
+
+//sajauc emoji nejaušā secībā
+let laukumiSajaukti = [...laukumiSaturs].sort(()=> Math.random)
